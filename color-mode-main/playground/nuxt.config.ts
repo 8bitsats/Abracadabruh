ec1ee09783bc21faae5cd7929d2f28955b89e6a5
8 bit <8bit@ordlibrary.com>
2024-05-31 22:47:26 -0400

    fff

---
+++ color-mode-main/playground/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  components: { global: true, dirs: ['~/components'] },
+  css: ['~/assets/main.css'],
+  modules: ['../src/module', '@nuxtjs/tailwindcss'],
+})
---
+++ color-mode-main/playground/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Title>Demo of @nuxtjs/color-mode</Title>
+    <ColorModePicker />
+    <p>
+      Go to
+      <NuxtLink to="/light">
+        light page
+      </NuxtLink>
+    </p>
+    <p>
+      Go to
+      <NuxtLink to="/tailwind">
+        tailwind integration
+      </NuxtLink>
+    </p>
+  </div>
+</template>
